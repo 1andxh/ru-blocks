0c08fe663998b5f3bb162d2352ed36a9ce39e928
andoh <solomonkingsman@gmail.com>
2026-04-15 23:04:18 +0000

    implement text block conversion

--- markdown_converter/md_converter.rb
+++ markdown_converter/md_converter.rb
@@ -13,6 +13,16 @@ class MarkdownConverter
         "<p>#{processed_text}</p>"
     end
 
+    def transform_text_block(multiple_lines)
+        lines = multiple_lines.split("\n")
+
+        lines_to_html = lines.map do |line|
+            convert(line)  
+        end
+
+        lines_to_html.join("/n")
+    end
+
     private
 
     def convert_header(text)
@@ -23,8 +33,22 @@ class MarkdownConverter
     def conver_bold(text)
         text.gsub(/\*\*(.*?)\*\*/, '<strong>\1</strong>')    
     end
+
+
 end
 
 converter = MarkdownConverter.new 
 puts converter.convert("# If you see this, it works!")
 puts converter.convert("# If you don't? it didn't work")
+
+test_markdown = <<~MARKDOWN
+  # Project Ruby
+  Ruby is **fast** and **fun**.
+  ## Getting Started
+  You just need to write **clean code**.
+  This is a regular paragraph without any flair.
+MARKDOWN
+
+
+puts "--- HTML OUTPUT ---"
+puts converter.transform_text_block(test_markdown)
